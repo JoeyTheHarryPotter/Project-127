5633ce424a10f2ca7a8941c2bafa0961a8302bd3
JoeyTheHarryPotter <74834315+JoeyTheHarryPotter@users.noreply.github.com>
2021-06-29 14:24:16 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+song = "";
+
+function preload(){
+    song = loadSound("Clubstep Full Version.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(500, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    song.play();
+}
+
+function draw(){
+    image(video, 0, 0, 500, 500);
+}
